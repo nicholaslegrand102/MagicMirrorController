c338f25a2584fc077d4549ae1bb0875e61d3ed3f
Nick <nicholaslegrand102@gmail.com>
2024-04-14 15:28:48 -0400

    Update index.ts
    add turn on turn off

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 const express = require("express");
 const path = require('path');
+const { exec } = require('node:child_process')
+
+
 const app = express();
 const port = 3000;
 
@@ -8,10 +11,35 @@ app.get("/", function (req, res) {
 });
 
 app.post("/On", function (req, res) {
+
+  // run the `ls` command using exec
+exec('vcgencmd display_power 1', (err, output) => {
+  // once the command has completed, the callback function is called
+  if (err) {
+      // log and return if we encounter an error
+      console.error("could not execute command: ", err)
+      return
+  }
+  // log the output received from the command
+  console.log("Output: \n", output)
+})
   res.send("On");
 });
 
 app.post("/Off", function (req, res) {
+
+    // run the `ls` command using exec
+exec('vcgencmd display_power 0', (err, output) => {
+  // once the command has completed, the callback function is called
+  if (err) {
+      // log and return if we encounter an error
+      console.error("could not execute command: ", err)
+      return
+  }
+  // log the output received from the command
+  console.log("Output: \n", output)
+})
+
   res.send("Off");
 });
 
